066522c3b8c15ec874e967d34a97b45983e2bf1c
PollyGlot <paul.trinko95@gmail.com>
2017-05-06 23:39:40 +0300

    Add Toolbar close button

--- app/src/main/java/com/example/pollyglot/apkplz/AddApk.java
+++ app/src/main/java/com/example/pollyglot/apkplz/AddApk.java
@@ -4,23 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.Window;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddApk extends AppCompatActivity {
 
-    private Toolbar mToolbarAddItem;
+    private Toolbar mToolbar;
+    private Spinner mSpinner;
+    private SpinnerAdapter mSpinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_apk);
 
-        mToolbarAddItem = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbarAddItem);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        TextView mTitle = (TextView) mToolbarAddItem.findViewById(R.id.toolbar_title);
+//        mSpinner = (Spinner) findViewById(R.id.choose_developer);
+
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
@@ -28,6 +46,11 @@ public class AddApk extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_add, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
 
