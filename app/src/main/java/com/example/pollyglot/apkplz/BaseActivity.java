d05380afb2705fae50dd4a043cab88fb4203b056
PollyGlot <paul.trinko95@gmail.com>
2017-05-19 01:03:41 +0300

    Add base activity to keep common tasks

---
+++ app/src/main/java/com/example/pollyglot/apkplz/BaseActivity.java
@@ -0,0 +1,31 @@
+package com.example.pollyglot.apkplz;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+}
