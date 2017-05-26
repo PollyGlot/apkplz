764082f7e4d4b2710aa6adf1d725ee033a4fc698
PollyGlot <paul.trinko95@gmail.com>
2017-05-27 00:54:44 +0300

    Code refactoring

--- app/src/main/java/com/example/pollyglot/apkplz/auth/LoginActivity.java
+++ app/src/main/java/com/example/pollyglot/apkplz/auth/LoginActivity.java
@@ -1,12 +1,8 @@
 package com.example.pollyglot.apkplz.auth;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -49,8 +45,8 @@ public class LoginActivity extends BaseActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
 
-    //Facebook callbackManager
-    private CallbackManager callbackManager;
+    //Facebook mCallbackManager
+    private CallbackManager mCallbackManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -98,10 +94,10 @@ public class LoginActivity extends BaseActivity {
             }
         };
 
-        callbackManager = CallbackManager.Factory.create();
+        mCallbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
         loginButton.setReadPermissions("email", "public_profile");
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "facebook:onSuccess:" + loginResult);
@@ -144,9 +140,10 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+
         email = (EditText) findViewById(R.id.login_email);
         password = (EditText) findViewById(R.id.login_password);
-        mFirebaseAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
@@ -161,7 +158,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     protected void setUpUser() {
