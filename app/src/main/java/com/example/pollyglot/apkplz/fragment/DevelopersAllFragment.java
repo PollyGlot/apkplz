8ea8f2a156bc9b968c7cd5063b1cf4e0c813246d
PollyGlot <paul.trinko95@gmail.com>
2017-06-12 14:07:53 +0300

    Query developers

--- app/src/main/java/com/example/pollyglot/apkplz/fragment/DevelopersAllFragment.java
+++ app/src/main/java/com/example/pollyglot/apkplz/fragment/DevelopersAllFragment.java
@@ -7,75 +7,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pollyglot.apkplz.R;
+import com.example.pollyglot.apkplz.adapters.DevAllListFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 
-public class DevelopersAllFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
+public class DevelopersAllFragment extends DevAllListFragment {
 
-    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
+    public DevelopersAllFragment() {}
 
-//    private OnFragmentInteractionListener mListener;
-
-    public DevelopersAllFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
-    public static DevelopersAllFragment newInstance(String param1, String param2) {
+    public static DevelopersAllFragment newInstance() {
         DevelopersAllFragment fragment = new DevelopersAllFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_developers_all, container, false);
     }
 
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
 
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+        Query devAllQuery = databaseReference.child("developers").orderByChild("developer");
+        return devAllQuery;
+    }
 }
--- app/src/main/java/com/example/pollyglot/apkplz/fragment/DevelopersTopFragment.java
+++ app/src/main/java/com/example/pollyglot/apkplz/fragment/DevelopersTopFragment.java
@@ -7,75 +7,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pollyglot.apkplz.R;
+import com.example.pollyglot.apkplz.adapters.DevTopListFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class DevelopersTopFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
+public class DevelopersTopFragment extends DevTopListFragment {
 
-    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
+    public DevelopersTopFragment() {}
 
-//    private OnFragmentInteractionListener mListener;
-
-    public DevelopersTopFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
     public static DevelopersTopFragment newInstance() {
-        DevelopersTopFragment fragment = new DevelopersTopFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
+        return new DevelopersTopFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_developers_top, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    @Override
+    public Query getQuery(final DatabaseReference databaseReference) {
 
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+        Query devTopQuery = databaseReference.child("developers").orderByChild("title").limitToFirst(10);
+        return devTopQuery;
+    }
 }
