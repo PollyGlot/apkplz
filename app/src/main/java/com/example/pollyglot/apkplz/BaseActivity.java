a2bd913fef8799953cd669401b8272785a9a4a01
PollyGlot <paul.trinko95@gmail.com>
2017-06-12 14:16:00 +0300

    Add Firebase offline mode

--- app/src/main/java/com/example/pollyglot/apkplz/BaseActivity.java
+++ app/src/main/java/com/example/pollyglot/apkplz/BaseActivity.java
@@ -1,13 +1,32 @@
 package com.example.pollyglot.apkplz;
 
 import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class BaseActivity extends AppCompatActivity {
 
     private ProgressDialog mProgressDialog;
+
+    private static String TAG = "BaseActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+            Log.d(TAG, FirebaseDatabase.getInstance().toString());
+        } catch (Exception e){
+            Log.w(TAG, "SetPersistenceEnabled:Fail" + FirebaseDatabase.getInstance().toString());
+            e.printStackTrace();
+        }
+    }
 
     public void showProgressDialog() {
         if (mProgressDialog == null) {
